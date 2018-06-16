341ea54c9422932c28a06c4d5d3d1ee518eb37d2
Uy Binh <uybinh@gmail.com>
2018-06-16 23:32:25 +0700

    Add email column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_16_155823) do
+ActiveRecord::Schema.define(version: 2018_06_16_162916) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2018_06_16_155823) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
