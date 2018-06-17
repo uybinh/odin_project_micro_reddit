d70bfb958442242f067a824048c42646c6afc025
Uy Binh <uybinh@gmail.com>
2018-06-17 08:46:09 +0700

    Add basic post, comment test

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -4,4 +4,14 @@ class CommentTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user = User.new(name: "Uy Binh", email: "binhphan@gmail.com", password: "binhphan", password_confirmation: "binhphan")
+    @post = @user.posts.build(title: "The great post", body: "Binh is here")
+    @comment = @post.comments.build(content: "Helle from comment content")
+  end
+
+  test "comment should valid" do
+    assert @comment.valid?
+  end
+
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -4,4 +4,13 @@ class PostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user = User.new(name: "Uy Binh", email: "binhphan@gmail.com", password: "binhphan", password_confirmation: "binhphan")
+    @post = Post.new(title: "The great post", body: "Binh is here")
+    @post.user = @user
+  end
+
+  test "post should be valid" do
+    assert @post.valid?
+  end
 end
